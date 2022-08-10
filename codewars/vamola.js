a23162d27b88a582717390a2471b9941607e50c1
GustavoPD <gudalmoro@gmail.com>
2022-08-10 16:36:31 -0300

    Decompress RLE list

---
+++ codewars/vamola.js
@@ -0,0 +1,11 @@
+function decompressRLElist(nums = []) {
+  let newArray = []
+    for (let index = 0; index < nums.length; index+= 2) {
+      for (let index2 = 0; index2 < nums[index]; index2++) {
+        newArray.push(nums[index+1]);
+      }
+    }
+    return newArray;
+};
+
+console.log(decompressRLElist([1, 2, 3, 4]));
